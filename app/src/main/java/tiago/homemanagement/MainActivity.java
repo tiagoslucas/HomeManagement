55ab56a683d2cbbd185ee4f93d84fdc32269bd83
tiagoslucas <tiago.slucas@gmail.com>
2018-06-29 00:20:29 +0100

    Added Settings activity

--- app/src/main/java/tiago/homemanagement/MainActivity.java
+++ app/src/main/java/tiago/homemanagement/MainActivity.java
@@ -30,8 +30,10 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
+        if (item.getItemId() == R.id.action_settings){
+            startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        }
         return super.onOptionsItemSelected(item);
     }
 
