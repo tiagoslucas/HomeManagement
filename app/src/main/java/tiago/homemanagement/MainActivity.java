734860daa28beff107bb8eb49a87549c833081d2
tiagoslucas <tiago.slucas@gmail.com>
2018-06-28 18:54:10 +0100

    Icons applied

--- app/src/main/java/tiago/homemanagement/MainActivity.java
+++ app/src/main/java/tiago/homemanagement/MainActivity.java
@@ -1,15 +1,13 @@
 package tiago.homemanagement;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,22 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                add();
-            }
-        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
         return true;
     }
 
@@ -43,36 +32,20 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
     public void laundry(View view) {
-        startActivity(LaundryActivity.class);
+        startActivity(new Intent(this,LaundryActivity.class));
     }
-
     public void shopping(View view) {
-        startActivity(ShoppingActivity.class);
+        startActivity(new Intent(this,ShoppingActivity.class));
     }
-
     public void floor(View view) {
-        startActivity(FloorActivity.class);
+        startActivity(new Intent(this,FloorActivity.class));
     }
-
     public void dishes(View view) {
-        startActivity(DishesActivity.class);
+        startActivity(new Intent(this,DishesActivity.class));
     }
-
-    public void add(){
-        startActivity(AddActivity.class);
-    }
-
-    public void startActivity(Class activity){
-        Intent intent = new Intent(this,activity);
-        this.startActivity(intent);
-    }
+    public void add(){ startActivity(new Intent(this,AddActivity.class)); }
 }
